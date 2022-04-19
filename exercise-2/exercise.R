c2492aeae3b22bd93a7549e4e1f2dae777336cb2
Khaitlin Bernaldez <ktbernaldez@gmail.com>
2022-04-18 22:13:36 -0700

    Complete Exercises

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,74 +2,96 @@
 
 # Install and load `ggplot2`
 # install.packages("ggplot2") # if needed
-
+install.packages("ggplot2")
+library(ggplot2)
 
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
-
-
+colnames(diamonds) # "carat" "cut" "color" "clarity" "depth" "table" "price" "x" "y" "z" 
+nrow(diamonds) # 53940
 
 ## Statistical Transformations
 
 # Draw a bar chart of the diamonds data, organized by cut
 # The height of each bar is based on the "count" (number) of diamonds with that cut
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut))
 
 # Use the `stat_count` to apply the statistical transformation "count" to the diamonds
 # by cut. You do not need a separate geometry layer!
+ggplot(data = diamonds) +
+  stat_count(mapping = aes(x = cut))
 
 
 # Use the `stat_summary` function to draw a chart with a summary layer.
 # Map the x-position to diamond `cut`, and the y-position to diamond `depth`
 # Bonus: use `min` as the function ymin, `max` as the function ymax, and `median` as the function y
-
+ggplot(data = diamonds) +
+  stat_summary(mapping = aes(x = cut, y = depth)) # function defaults to mean_se()
 
 
 ## Position Adjustments
 
 # Draw a bar chart of diamond data organized by cut, with each bar filled by clarity.
 # You should see a _stacked_ bar chart.
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity))
 
 # Draw the same chart again, but with each element positioned to "fill" the y axis
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "fill")
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
-
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = clarity), position = "dodge")
 
 # Draw a plot with point geometry with the x-position mapped to `cut` and the y-position mapped to `clarity`
 # This creates a "grid" grouping the points
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = cut, y = clarity))
 
 # Use the "jitter" position adjustment to keep the points from all overlapping!
 # (This works a little better with a sample of diamond data, such as from the previous exercise).
-
-
+ggplot(data = diamonds) +
+  geom_jitter(mapping = aes(x = cut,y = clarity))
 
 ## Scales
 
 # Draw a "boxplot" (with `geom_boxplot()`) for the diamond's price (y) by color (x)
-
+ggplot(data = diamonds) +
+  geom_boxplot(mapping = aes(x = color, y = price))
 
 # This has a lot of outliers, making it harder to read. To fix this, draw the same plot but
 # with a _logarithmic_ scale for the y axis.
-
+ggplot(data = diamonds) +
+  geom_boxplot(mapping = aes(x = color, y = log(price)))
 
 # For another version, draw the same plot but with `violin` geometry instead of `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
+ggplot(data = diamonds) +
+  geom_violin(mapping = aes(x = color, y = price))
 
+ggplot(data = diamonds) +
+  geom_violin(mapping = aes(x = color, y = log(price))) # makes the body of the violin wider
 
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), using a heatmap of 2d bins
 # (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
+ggplot(data = diamonds) +
+  geom_bin2d(mapping = aes(x = carat, y = price))
 
+ggplot(data = diamonds) +
+  geom_bin2d(mapping = aes(x = log(carat), y = log(price))) # linear
 
 # Draw a scatter plot for the diamonds  price (y) by carat (x). Color each point by the clarity
 # (Remember, this will take a while. Use a sample of the diamonds for faster results)
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity))
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. What looks nice?
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = 3)
 
 
 ## Coordinate Systems
@@ -77,24 +99,30 @@
 # Draw a bar chart with x-position and fill color BOTH mapped to cut
 # For best results, SET the `width` of the geometry to be 1 (fill plot, no space between)
 # You can save this to a variable for easier modifications
-
+bar_chart <- ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = cut), width = 1)
 
 # Draw the same chart, but with the coordinate system flipped
-
+bar_chart +
+  coord_flip()
 
 # Draw the same chart, but in a polar coordinate system. Now you have a Coxcomb chart!
-
+bar_chart +
+  coord_polar(theta = "y")
 
 
 ## Facets
 
 # Take the scatter plot of price by carat data (colored by clarity) and add _facets_ based on
 # the diamond's `color`
-
-
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  facet_wrap(~color)
 
 ## Saving Plots
 
 # Use the `ggsave()` function to save one of your plots (the most recent one generated) to disk.
 # Name the output file "my-plot.png".
 # Make sure you've set the working directory!!
+ggsave(my_plot.png)
+
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,21 +9,25 @@ library("dplyr")
 election <- read.csv('data/2016_US_County_Level_Presidential_Results.csv', stringsAsFactors = FALSE)
 
 # Inspect the `election` data frame to understand the data you're working with
-
+colnames(election) # "X" "votes_dem" "votes_gop" "total_votes" "per_dem" "per_gop" "diff" "per_point_diff" "state_abbr" "county_name" "combined_fips" 
+head(election)
+nrow(election) #3141
 
 # Consider: what column contains state names? What column contains county names?
 # What format are those county names in?
-
+## State Names: state.abbr (str)
+## County Names: county_name (str)
 
 # Use `map_data()` to load the `county` map of the US, storing it in a variable
-
+counties <- map_data("county")
 
 # Inspect this data frame to understand what data yu're working with
-
+View(us.states)
 
 # Consider: what column contains state names? What column contains county names?
 # What format are those county names in?
-
+## State Names: region
+## County Names: subregion
 
 ### Data Wrangling
 
@@ -33,37 +37,52 @@ election <- read.csv('data/2016_US_County_Level_Presidential_Results.csv', strin
 # available from the `maps` library (which you should install and load)
 
 # Use `data()` to load the `"county.fips"` data frame into the environment (does not return anything)
-
+data("county.fips")
 
 # Inspect the `county.fips` data frame to see what you got
-
+View(county.fips)
 
 # Use a `join` operation to add the `fips` column to your `counties` data frame.
 # Note that you may need to use `paste0()` and `mutate() to make a column of "state,county"
 # to join by!
 # Note: don't worry about Alaska for this exercise.
+counties <- mutate(counties, polyname = paste0(region, ",", subregion))
 
+counties <- full_join(counties, county.fips, by = c("polyname" = "polyname"))
 
 # Now you can join the `counties` map data (with fips!) to the `election` data
 # Hint: use `by = c("fips" = "combined_fips")` to specify the column to join by
-
+election <- full_join(counties, election, by = c("fips" = "combined_fips"))
 
 # One more change: add a column to store whether the Democrat or the Republication party
 # had the higher number of votes ("won" the county)
 # Hint: start by adding a column of logical values (TRUE/FALSE) of whether a party one,
 #       and then join that with a simple data frame that matches to Strings
+election <- mutate(election, Won = votes_dem > votes_gop)
 
+boolean <- c(TRUE, FALSE)
+Party <- c("Democrats", "Republicans")
+winner <- data.frame(boolean, Party)
 
+election <- full_join(election, winner, by = c("Won" = "boolean"))
 
 ### Data Visualization
 
 # Finally, plot the polygons of the map!
 # Be sure and specify the `x` (longitude), `y` (latitude), and `group` (group) asthetics
 # Use the `coord_quick_map()` coordinate system to make it look nice
-
+ggplot(data = election) +
+  geom_polygon(aes(x = long, y = lat, group = group)) +
+  coord_quickmap()
 
 # Modify the above plot so that each geometry is filled based on which party won the county
 # Specify a `manual` fill scale to make Democratic counties "blue" and Republican counties "red"
+cols <- c("Democrats" = "blue", "Republicans" = "red", "NA" = "black")
+
+ggplot(data = election) +
+  geom_polygon(aes(x = long, y = lat, group = group, fill = factor(Party))) +
+  coord_quickmap() +
+  scale_color_manual(values = cols, aesthetics = "fill")
 
 
 # For fun: how else can you fill in this map? What other insights can you produce?
